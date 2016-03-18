dd26f42f3c4804696b9de43dadd8d6b8b8e308df
tsolyanVolusion <tom_solyan@volusion.com>
2016-03-18 09:49:45 -0500

    remove lib

--- reportingApp/ios/reportingApp/views/Landing.js
+++ reportingApp/ios/reportingApp/views/Landing.js
@@ -12,21 +12,7 @@ const {
   Component
 } = React;
 
-import { setTheme, MKColor } from 'react-native-material-kit';
 
-// customize the material design theme
-setTheme({
-  primaryColor: MKColor.Purple,
-  primaryColorRGB: MKColor.RGBPurple,
-  accentColor: MKColor.Amber,
-});
-
-let Buttons = require('../../../MDcomponents/buttons');
-let TextFields = require('../../../MDcomponents/textfields');
-let Toggles = require('../../../MDcomponents/toggles');
-let Progress = require('../../../MDcomponents/progress');
-let Sliders = require('../../../MDcomponents/sliders');
-let Cards = require('../../../MDcomponents/cards');
 
 class Landing extends Component{
     constructor(props) {
@@ -38,100 +24,20 @@ class Landing extends Component{
     }
   render() {
    return (
-      <ScrollView style={styles.list}
-                  contentContainerStyle={styles.container}>
-        <TouchableOpacity onPress={() => {
-          this.props.navigator.push({
-            title: 'Buttons',
-            component: Buttons,
-          });
-        }}>
-          <Text style={styles.pushLabel}>Buttons</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => {
-          this.props.navigator.push({
-            title: 'Cards',
-            component: Cards,
-          });
-        }}>
-          <Text style={styles.pushLabel}>Cards</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => {
-          this.props.navigator.push({
-            title: 'Loading',
-            component: Progress,
-          });
-        }}>
-          <Text style={styles.pushLabel}>Loading</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => {
-          this.props.navigator.push({
-            title: 'Sliders',
-            component: Sliders,
-          });
-        }}>
-          <Text style={styles.pushLabel}>Sliders</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => {
-          this.props.navigator.push({
-            title: 'Text Fields',
-            component: TextFields,
-          });
-        }}>
-          <Text style={styles.pushLabel}>Text Fields</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => {
-          this.props.navigator.push({
-            title: 'Toggles',
-            component: Toggles,
-          });
-        }}>
-          <Text style={styles.pushLabel}>Toggles</Text>
-        </TouchableOpacity>
-      </ScrollView>
+    <View style={styles.greet}>
+      <Text > Howdy!! </Text>
+    </View>
     );
 
     }
 };
 
-var Example = React.createClass({
-  render: function () {
-    return (
-      <NavigatorIOS
-        style={{flex:1}}
-        initialRoute={{
-          component: Landing,
-          title: 'Examples',
-        }}
-      />
-    );
-  },
-});
-
-var styles = StyleSheet.create({
-  list: {
-    backgroundColor: '#F5FCFF',
-    paddingTop: 20,
-  },
-  container: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginTop: 20, marginBottom: 0,
-  },
-  pushLabel: {
-    padding: 10,
-    color: '#2196F3',
-  }
-});
-
-
+const styles = StyleSheet.create({
+    greet: {
+        flex: 1,
+        paddingTop: 40,
+        alignItems: 'center',
+        padding: 10
+    }
+  });
 module.exports = Landing;
